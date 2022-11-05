71d5f45fde51cb76e3e1112a38579a92abe48bbb
DirectiveAthena <directive.athena@gmail.com>
2022-11-05 15:59:56 +0100

    Feat: Django foundation

---
+++ manage.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import sys
+from django.core.management import execute_from_command_line
+
+# Athena Packages
+from AthenaLib.parsers.dot_env import DotEnv
+
+# Local Imports
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def main():
+    """Run administrative tasks."""
+    # Gather all env variables from the secrets folder
+    DotEnv("secrets/portus_spatium_api.env", auto_run=True)
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
