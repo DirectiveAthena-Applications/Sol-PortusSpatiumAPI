dbe10ea3d41cf70b00c4f6c6653544d8162d937e
DirectiveAthena <directive.athena@gmail.com>
2022-11-06 14:32:43 +0100

    Fix: rename portus_spatium with `_api`

--- src/sol/portus_spatium/settings.py
+++ src/sol/portus_spatium/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portus_spatium_api.urls'
+ROOT_URLCONF = 'portus_spatium.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portus_spatium_api.wsgi.application'
+WSGI_APPLICATION = 'portus_spatium.wsgi.application'
 
 
 # Database
